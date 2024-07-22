f647f3b4106943aff9a61ffbd084b72907267acc
Saku <sakurho@gmail.com>
2024-07-22 14:34:57 +0300

    Stop notifications when adding a new plugin file.

--- lua/saku/lazy.lua
+++ lua/saku/lazy.lua
@@ -11,4 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("lazy").setup("saku.plugins", {
+  change_detection = {
+    notify = false,
+  },
+}
+
 require("lazy").setup("saku.plugins")
